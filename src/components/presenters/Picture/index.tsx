5d306fd49de35e0c384b23348a15fd2a2e2a06e4
Akihiro Tojo <akihiro.021109@gmail.com>
2023-12-26 00:40:44 +0900

    Add animation

--- src/components/presenters/Picture/index.tsx
+++ src/components/presenters/Picture/index.tsx
@@ -1,22 +1,40 @@
+import { animated, useSpring } from '@react-spring/three';
 import { useAspect, useTexture } from '@react-three/drei';
+import { useEffect } from 'react';
 import { DoubleSide } from 'three';
 
 type PictureProps = {
-  position: [number, number, number];
-  imgURL: string;
+  url: string;
+  position: {
+    from: [number, number, number];
+    to: [number, number, number];
+  };
   scaleFactor: number;
 };
 
-const Picture = ({ position, imgURL, scaleFactor }: PictureProps) => {
-  const texture = useTexture(imgURL);
+const Picture = ({ url, position, scaleFactor }: PictureProps) => {
+  const texture = useTexture(url);
   const { width, height } = texture.source.data;
   const scale = useAspect(width, height, scaleFactor);
 
+  const [springs, api] = useSpring(() => ({}));
+
+  useEffect(() => {
+    api.start({
+      from: {
+        position: position.from,
+      },
+      to: {
+        position: position.to,
+      },
+    });
+  }, [api, position]);
+
   return (
-    <mesh position={position} scale={scale}>
+    <animated.mesh position={springs.position} scale={scale}>
       <planeGeometry />
       <meshStandardMaterial side={DoubleSide} map={texture} />
-    </mesh>
+    </animated.mesh>
   );
 };
 
--- src/components/presenters/ThreeCanvas/index.tsx
+++ src/components/presenters/ThreeCanvas/index.tsx
@@ -1,41 +1,54 @@
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
 
 import Picture from '../Picture';
 
 const RADIUS = 2;
-const PICTURE_SCALE = 0.15;
+const SCALE_FACTOR = 0.15;
+
+const POSITIONS = [
+  [RADIUS * Math.cos(0), 0, 0],
+  [0, 0, RADIUS * 1.5 * Math.sin(Math.PI / 2)],
+  [RADIUS * Math.cos(Math.PI), 0, 0],
+] as [number, number, number][];
+
+const IMG_URLS = ['./img/morning.jpeg', './img/noon.jpeg', './img/sunset.jpeg'];
 
 const CanvasContent = () => {
+  const [imgURLs, setImgURLs] = useState(IMG_URLS);
+
+  useEffect(() => {
+    const intervalID = window.setInterval(() => {
+      setImgURLs(prev => [prev.slice(-1)[0], ...prev.slice(0, -1)]);
+    }, 2000);
+    return () => window.clearInterval(intervalID);
+  }, []);
+
   return (
     <>
-      <PerspectiveCamera position={[0, 0, 6]} makeDefault />
-      <OrbitControls />
-
-      <ambientLight />
-
-      <Picture
-        position={[0, 0, RADIUS * 1.5 * Math.sin(Math.PI / 2)]}
-        imgURL="./img/morning.jpeg"
-        scaleFactor={PICTURE_SCALE}
-      />
-      <Picture
-        position={[RADIUS * Math.cos(0), 0, 0]}
-        imgURL="./img/noon.jpeg"
-        scaleFactor={PICTURE_SCALE}
-      />
-      <Picture
-        position={[RADIUS * Math.cos(Math.PI), 0, 0]}
-        imgURL="./img/sunset.jpeg"
-        scaleFactor={PICTURE_SCALE}
-      />
-
       {import.meta.env.DEV && (
         <>
           <gridHelper />
           <axesHelper args={[5]} />
+          <OrbitControls />
         </>
       )}
+
+      <PerspectiveCamera position={[0, 0, 6]} makeDefault />
+      <ambientLight />
+
+      {imgURLs.map((url, index) => (
+        <Picture
+          key={index}
+          url={url}
+          scaleFactor={SCALE_FACTOR}
+          position={{
+            from: POSITIONS[index - 1 > -1 ? index - 1 : POSITIONS.length - 1],
+            to: POSITIONS[index],
+          }}
+        />
+      ))}
     </>
   );
 };
